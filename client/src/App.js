2348fb3dd7bf65c418d7199a99d8361cdca1d7f7
unknown <ksbohyun@naver.com>
2020-10-20 19:28:01 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,25 @@ const styles = (theme) => ({
   },
 });
 
-const customer = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "김보현",
-    birthday: "930820",
-    gender: "남자",
-    job: "무직",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "강수현",
-    birthday: "920225",
-    gender: "여자",
-    job: "자영업",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "김근현",
-    birthday: "981212",
-    gender: "남자",
-    job: "군인",
-  },
-];
-
 class App extends React.Component {
+  state = {
+    customers: [],
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -64,19 +55,21 @@ class App extends React.Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customer.map((customer) => {
-              return (
-                <Customer
-                  key={customer.id}
-                  id={customer.id}
-                  image={customer.image}
-                  name={customer.name}
-                  birthday={customer.birthday}
-                  gender={customer.gender}
-                  job={customer.job}
-                />
-              );
-            })}
+            {this.state.customers
+              ? this.state.customers.map((customer) => {
+                  return (
+                    <Customer
+                      key={customer.id}
+                      id={customer.id}
+                      image={customer.image}
+                      name={customer.name}
+                      birthday={customer.birthday}
+                      gender={customer.gender}
+                      job={customer.job}
+                    />
+                  );
+                })
+              : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "김보현",
+      birthday: "930820",
+      gender: "남자",
+      job: "무직",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "강수현",
+      birthday: "920225",
+      gender: "여자",
+      job: "자영업",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "김근현",
+      birthday: "981212",
+      gender: "남자",
+      job: "군인",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
