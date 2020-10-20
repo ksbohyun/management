0102e981b8e59d98702d20f9437c5cac9b0ff375
unknown <ksbohyun@naver.com>
2020-10-20 23:40:11 +0900

    Add Material UI Design for Add/Delete

--- server.js
+++ server.js
@@ -55,7 +55,6 @@ app.post("/api/customers", upload.single("image"), (req, res) => {
   connection.query(sql, params, (err, rows, fields) => {
     res.send(rows);
     console.log(err);
-    console.log(date);
   });
 });
 
